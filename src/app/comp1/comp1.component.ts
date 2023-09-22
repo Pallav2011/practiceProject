968334f33cb88b4315d52a4ca29e5485bda66d61
Pallav2011 <ranpisepg09@gmail.com>
2023-09-22 23:01:36 +0530

    comp1 is created
    comp1 is created for understanding the data binding techniques in angular.

---
+++ src/app/comp1/comp1.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comp1',
+  templateUrl: './comp1.component.html',
+  styleUrls: ['./comp1.component.css']
+
+})
+export class Comp1Component implements OnInit {
+
+  constructor() { }
+
+  developerName:string = 'Pandurang';
+  imageSource = 'https://statusneo.com/wp-content/uploads/2023/02/MicrosoftTeams-image551ad57e01403f080a9df51975ac40b6efba82553c323a742b42b1c71c1e45f1.jpg';
+  value : string = 'pandurang Ranpise';
+
+  ngOnInit() {
+  }
+
+  changeImage(){
+    this.imageSource = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVIkeiwbu6hn5zP31DgzDZ0VnJhRf6jYyC8A&usqp=CAU';
+  }
+
+}
